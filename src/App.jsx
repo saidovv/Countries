1abb46fb299378188ab78b5dfb2f904fd6f03142
saidovv <116736002+saidovv@users.noreply.github.com>
2023-06-29 15:33:42 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Countries from "./components/Countries";
+import SingleCountry from "./components/SingleCountry";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Countries />}></Route>
+          <Route path="/:name" element={<SingleCountry />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
